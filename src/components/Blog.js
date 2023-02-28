60e938223f2c653601999dacdfa2159afd6ae6fd
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-02-28 21:50:25 +0200

    Part 5 bloglist frontend step 9

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false)
@@ -16,9 +17,10 @@ const Blog = ({ blog }) => {
       <p>{blog.url}</p>
       <div>
         likes {blog.likes}
-        <button>like</button>
+        <button onClick={handleLikeClick}>like</button>
       </div>
       <p>{blog.user.name}</p>
+      <button onClick={handleRemove}>remove</button>
     </div>
   )
 
@@ -29,6 +31,23 @@ const Blog = ({ blog }) => {
       setButtonText('view')
     }
     setShowDetails(!showDetails)
+  }
+
+  const handleLikeClick = (event) => {
+    event.preventDefault()
+    const updatedBlogObject = {
+      user: blog.user._id,
+      likes: blog.likes + 1,
+      author: blog.author,
+      title: blog.title,
+      url: blog.url
+    }
+    console.log(blog)
+    blogService.update(blog.id, updatedBlogObject)
+  }
+
+  const handleRemove = () => {
+    // blogService.remove(blog.id)
   }
 
   return (
